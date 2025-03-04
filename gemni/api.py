fbf8e0a262902435ae805cb4a695da556582dd40
Arpit <omagrahari55@gmail.com>
2025-03-04 15:42:34 +0530

    created a ai chatbot

---
+++ gemni/api.py
@@ -0,0 +1,29 @@
+import google.generativeai as genai
+import os
+
+# client = genai("AIzaSyB4nPKaYh8gYiPyKWTzo9muNojNX55VQK8")
+
+genai.configure(api_key='AIzaSyB4nPKaYh8gYiPyKWTzo9muNojNX55VQK8')
+
+model = genai.GenerativeModel(model_name='gemini-1.5-flash')
+
+def askai(query):
+    question = """
+    Query asked {}
+    START CONTEXT
+    -->When user ask who created you than only tell them
+    -->Hey you are an ai made by arpit. Arpit is a full stack dev how created you answer the query asked by the user in 1-2 line not more than that 
+    -->Your name is ChotuAi
+    END CONTEXT
+    """.format(query)
+    response = model.generate_content(question)
+    print("AI:- ",response.text)
+print("To Exit Press 1 \n")
+print("---------Welcome----------------")
+while True:
+    userinput = input("Enter the message :- ")
+    if userinput == "1":
+        break
+    askai(userinput)
+
+print("---------Byeee----------------")
